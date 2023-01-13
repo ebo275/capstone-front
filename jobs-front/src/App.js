8fdd7d3c7fcc7d97a5c3fc8c8430bf55a74aa3af
ebo275 <115588483+ebo275@users.noreply.github.com>
2023-01-13 14:29:05 -0800

    redux added

--- jobs-front/src/App.js
+++ jobs-front/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
+import { useSelector } from 'react-redux';
+// import Add from './components/Add.js'
+// import Edit from './components/Edit.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+
+// READING
+const [job, setJob] = useState([])
+const getJob = () => {
+  axios.get('https://capstone-be.herokuapp.com/api/jobs').then((res) => {
+    console.log(res.data)
+    setJob(res.data)
+  })
 }
 
+useEffect(() => {
+  getJob()
+}, [])
+
+// REDUX
+const { count } = useSelector((state) => state.counter.count);
+
+  return (
+    <>
+      <h1>Ella's Job Search Tracker</h1>
+      <div className = 'job'>
+        {job.map((job)=> {
+          return (
+            <div className='jobs' key={'resolution.id'}>
+              <h4>Title: {job.title}</h4>
+              <h4>Company: {job.company}</h4>
+              <h4>Salary: {job.salary}</h4>
+              <h4>Date Applied: {job.dateApplied}</h4>
+              <h4>Response: {job.response}</h4>
+
+
+            </div>
+        )
+      })}
+      </div>
+    
+    </>  
+  );
+  
+}
 export default App;
